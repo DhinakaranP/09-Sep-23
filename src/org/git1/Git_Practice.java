06ce51eefaffce3e1f407c05aa4630f804aa3024
DhinakaranP <sarathidhina@gmail.com>
2023-09-09 22:03:52 +0530

    sub

--- src/org/git1/Git_Practice.java
+++ src/org/git1/Git_Practice.java
@@ -2,23 +2,14 @@ package org.git1;
 
 public class Git_Practice {
 	
-private void add() {
-	
+private void add() {	
 	System.out.println("add");
-
 }
-
 private void sub() {
-	
 	System.out.println("Subtraction");
-
 }
-
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 	Git_Practice obj=new Git_Practice();
-	
 	obj.add();
 	obj.sub();
 }
