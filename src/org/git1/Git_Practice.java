2eba26257e5e1d64aa44ec35087866bd391ecb46
DhinakaranP <sarathidhina@gmail.com>
2023-09-09 21:24:28 +0530

    add method add

---
+++ src/org/git1/Git_Practice.java
@@ -0,0 +1,18 @@
+package org.git1;
+
+public class Git_Practice {
+	
+private void add() {
+	
+	System.out.println("add");
+
+}
+
+	public static void main(String[] args) {
+		
+	Git_Practice obj=new Git_Practice();
+	
+	obj.add();
+}
+	
+}
